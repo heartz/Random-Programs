d66a7959d75c8de8c9e5e4c459c12ea448c7cbd2
Jaiswal-ruhil <ruhiljaiswal@gmail.com>
2014-03-25 00:02:48 +0530

    Update Reverse_sentence.py
    well the proper use of string and using less space by terminating sentance

--- Googlecodejam/Reverse_sentence.py
+++ Googlecodejam/Reverse_sentence.py
@@ -1,14 +1,8 @@
-
 fil=open("output1.txt","w+")
 fil2=open("B-large-practice.in","r")
 noOfTestCases=int(fil2.readline())
 for i in range(1,noOfTestCases+1):
-    sentence=fil2.readline()
-    wordlist=sentence.split()
+    wordlist=fil2.readline().split()
     wordlist=wordlist[::-1]
     c=' '.join(wordlist)
-    fil.write("Case #")
-    fil.write(str(i))
-    fil.write(": ")
-    fil.write(c)
-    fil.write('\n')
+    fil.write("Case #"+str(i)+": "+c+'\n')
