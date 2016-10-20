367c045846e828f3f41a34fca606618b3f564ded
Ranadip Saha <ranadip.rulz@gmail.com>
2016-10-20 19:32:13 +0530

    Fix bugs in the logic

--- Brainfuck/EnglishtoBF.py
+++ Brainfuck/EnglishtoBF.py
@@ -1,17 +1,25 @@
 s1=input()
 s2=list(s1)
 s3=[ord(j) for j in s2]
+loop_list = []
+remain_list = []
+print("++++++++++")
+print("[",end="")
 for i in range(0,len(s2)):
     loop=int(s3[i]/10)
-    remain=s3[i]%10
+    loop_list.append(loop)
+    print(">",end="")
     for j in range(0,loop):
         print("+",end="")
-    print()
-    print("[>++++++++++<-]")
+for i in range(0,len(s2)):
+    print("<",end="")
+print("-]")
+for i in range(0,len(s2)):
     print(">",end="")
+    remain=s3[i]%10
+    remain_list.append(remain)
     for j in range(0,remain):
         print("+",end="")
     print(".")
-    print("<<")
     
         
